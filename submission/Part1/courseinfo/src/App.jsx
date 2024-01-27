1430ce91fca7cfa9b4c3975098466ec049246e88
Sohinisahukar <sohini.sahukar@gmail.com>
2024-01-27 19:54:07 +0530

    Part1 exercise courseinfo initial commit

---
+++ submission/Part1/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.header}</h1>
+    </>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </>
+  )
+}
+
+const Total = ({ parts }) => {
+  var total = 0
+  parts.forEach(part => {
+    total += part.exercises
+  })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header header={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
